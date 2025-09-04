3e7070aad1d243a88d68e5cdb7c5d6ac855e631f
udaygdev <ugg1@scarletmail.rutgers.edu>
2025-09-04 16:37:27 -0400

    Added looping of program so we don't have to keep restarting program. Added a gitignore to ignore a todo list.

--- main.c
+++ main.c
@@ -1,51 +1,70 @@
-//This is just a simple calculator used for my own understanding of some principles.
-//It has little to no safeguards in place, so overflow and wrong type of inputs will cause errors.
-//I might change it in the near future to account for different types or overflow. Otherwise, this is just for my own understanding.
+/*
+This is just a simple calculator used for my own understanding of some principles.
+It has little to no safeguards in place, so overflow and wrong type of inputs will cause errors.
+I might change it in the near future to account for different types or overflow. Otherwise, this is just for my own understanding.
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(){
+    int cont = 1;
+    char choice;
     
-    int a;
-    int b;
-    char op;
+    do{
+        int a;
+        int b;
+        char op;
 
-    printf("Type INTEGER 1: ");
-    scanf("%d", &a);
-    printf("\n");
+        printf("Type INTEGER 1: ");
+        scanf("%d", &a);
+        printf("\n");
 
-    printf("Type INTEGER 2: ");
-    scanf("%d", &b);
-    printf("\n");
+        printf("Type INTEGER 2: ");
+        scanf("%d", &b);
+        printf("\n");
 
-    
-    printf("Type operator (+, -, *, /, %%): ");
-    scanf(" %c", &op); //ignore leading space.
-    printf("\n");
+        
+        printf("Type operator (+, -, *, /, %%): ");
+        scanf(" %c", &op); //ignore leading space.
+        printf("\n");
 
-    switch(op){
-        case '+':
-            printf("%d", a+b);
-            break;
-        case '-':
-            printf("%d", a-b);
-            break;
-        case '*':
-            printf("%d", a*b);
-            break;
-        case '/':
-            printf("%d", a/b);
-            break;
-        case '%':
-            printf("%d", a%b);
-            break;
-        default:
-            printf("Invalid Operator\n");
-            break;
-    }
+        switch(op){
+            case '+':
+                printf("%d", a+b);
+                break;
+            case '-':
+                printf("%d", a-b);
+                break;
+            case '*':
+                printf("%d", a*b);
+                break;
+            case '/':
+                printf("%d", a/b);
+                break;
+            case '%':
+                printf("%d", a%b);
+                break;
+            default:
+                printf("Invalid Operator\n");
+                break;
+        }
+        printf("\n");
 
-    printf("\n");
+        printf("Would you like to continue? (y/n): ");
+        scanf(" %c", &choice);
+
+        if(choice == 'Y' || choice == 'y'){
+            printf("Continuing \n\n");
+        }
+        else{
+            cont = 0;
+            if(choice != 'N' && choice != 'n'){
+                printf("\nInvalid entry. Terminating program\n");
+            }
+        }
+
+    }while(cont);
 
 
     return 0;
