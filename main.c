24b0c406d67d6147235b5a4444cd34cf6f1020e6
udaygdev <ugg1@scarletmail.rutgers.edu>
2025-09-04 14:54:13 -0400

    Initial Dead Simple Calculator Project

---
+++ main.c
@@ -0,0 +1,49 @@
+//This is just a simple calculator used for my own understanding of some principles.
+//It has little to no safeguards in place, so overflow and wrong type of inputs will cause errors.
+//I might change it in the near future to account for different types or overflow. Otherwise, this is just for my own understanding.
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    
+    int a;
+    int b;
+    char op;
+
+    printf("Type INTEGER 1: ");
+    scanf("%d", &a);
+    printf("\n");
+
+    printf("Type INTEGER 2: ");
+    scanf("%d", &b);
+    printf("\n");
+
+    
+    printf("Type operator (+, -, *, /, %%): ");
+    scanf(" %c", &op); //ignore leading space.
+    printf("\n");
+
+    switch(op){
+        case '+':
+            printf("%d", a+b);
+            break;
+        case '-':
+            printf("%d", a-b);
+            break;
+        case '*':
+            printf("%d", a*b);
+            break;
+        case '/':
+            printf("%d", a/b);
+            break;
+        default:
+            printf("Invalid Operator\n");
+            break;
+    }
+
+    printf("\n");
+
+
+    return 0;
+}
